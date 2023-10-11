390ce447caaec1ec462543ca6ad98acf115759bd
Emeraudia <stevan_metayer@orange.fr>
2023-10-11 11:10:13 +0200

    Rename window / Set default text

--- OMD/TP2/V1_imp/src/Window.java
+++ OMD/TP2/V1_imp/src/Window.java
@@ -1,45 +1,41 @@
-
-
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
-
-
 public class Window {
 
-    private JLabel l;
-    private JFrame f;
-    private static Window singleton;
-    
-    protected Window(){
-        f = new JFrame("Demo");
-        f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
-	    f.setSize(500, 500);
-        
-	    l = new JLabel();
-	    l.setText("This is a demonstration");
-	    f.add(l);
-	    f.setVisible(true);
-        f.addKeyListener(Input.getInstance());
+  private JLabel l;
+  private JFrame f;
+  private static Window singleton;
 
-       f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  protected Window() {
+    f = new JFrame("App");
+    f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
+    f.setSize(500, 500);
+
+    l = new JLabel();
+    printToWindow();
+    f.add(l);
+    f.setVisible(true);
+    f.addKeyListener(Input.getInstance());
+
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 
-  public static Window getInstance(){
-    if(singleton == null){
+  public static Window getInstance() {
+    if (singleton == null) {
       singleton = new Window();
     }
     return singleton;
   }
 
-  public void update(){
+  public void update() {
     printToWindow();
   }
 
-  private void printToWindow(){
-        l.setText(ZoneDeTravail.getInstance().getTexte());
-  }
+  private void printToWindow() {
     
+    l.setText(ZoneDeTravail.getInstance().getTexte());
+  }
+
 }
