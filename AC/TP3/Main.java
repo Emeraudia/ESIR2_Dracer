25216b6e9dd7b608aba94f93072cf7868b6ce261
Emeraudia <stevan_metayer@orange.fr>
2023-12-05 10:12:22 +0100

    Ex2

--- AC/TP3/Main.java
+++ AC/TP3/Main.java
@@ -8,19 +8,36 @@ public class Main {
 
 	public static void main(String[] args) {
 		//EXEMPLE 
-		Expression exp = new Et(new Atome("x"),new Atome("y")); // représente (x ^ y)
-		System.out.println(exp.atomes()); // affiche la liste des atomes (=variables booléennes) présents dans exp
-		exp = exp.remplace("x",true); // exp vaut maintenant (true ^ y)
-		System.out.println(exp.evalue()); // <- erreur car (true ^ y) ne peut pas être évalué
-		exp = exp.remplace("y",false); // exp vaut maintenant (true ^ false)
-		System.out.println(exp.evalue());
+		// Expression exp = new Et(new Atome("x"),new Atome("y")); // représente (x ^ y)
+		// System.out.println(exp.atomes()); // affiche la liste des atomes (=variables booléennes) présents dans exp
+		// exp = exp.remplace("x",true); // exp vaut maintenant (true ^ y)
+		// System.out.println(exp.evalue()); // <- erreur car (true ^ y) ne peut pas être évalué
+		// exp = exp.remplace("y",false); // exp vaut maintenant (true ^ false)
+		// System.out.println(exp.evalue());
 		
-		//Affichage de l'arbre associé à l'expression exp pour l'ordre x > y 
+		// //Affichage de l'arbre associé à l'expression exp pour l'ordre x > y 
+		// List<String> ordre_atomes = new LinkedList<String>();
+		// ordre_atomes.add("x");
+		// ordre_atomes.add("y");
+		// System.out.println("\n Arbre de exp : \n" + exp.arbre(ordre_atomes)); // <- que se passe-t-il ? 
+		// Expression exp2 = new Et(new Atome("x"),new Atome("y")); // représente (x ^ y)
+		// System.out.println("\n Arbre de exp2 : \n" + exp2.arbre(ordre_atomes));		
+
+		Expression exp = new Et(new Equiv(new Atome("x1"), new Atome("y1")), new Equiv(new Atome("x2"), new Atome("y2")));
+		System.out.println(exp.atomes());
+		exp = exp.remplace("x1",false);
+		exp = exp.remplace("y1",true);
+		exp = exp.remplace("x2",false);
+		exp = exp.remplace("y2",false);
+		System.out.println(exp.evalue()); // (Faux <=> Vrai) ET (Faux <=> Faux)
+		
+				
+		Expression exp1 = new Et(new Equiv(new Atome("x1"), new Atome("y1")), new Equiv(new Atome("x2"), new Atome("y2")));
 		List<String> ordre_atomes = new LinkedList<String>();
-		ordre_atomes.add("x");
-		ordre_atomes.add("y");
-		System.out.println("\n Arbre de exp : \n" + exp.arbre(ordre_atomes)); // <- que se passe-t-il ? 
-		Expression exp2 = new Et(new Atome("x"),new Atome("y")); // représente (x ^ y)
-		System.out.println("\n Arbre de exp2 : \n" + exp2.arbre(ordre_atomes));		
+		ordre_atomes.add("x1");
+		ordre_atomes.add("x2");
+		ordre_atomes.add("y1");
+		ordre_atomes.add("y2");
+		System.out.println("\n Arbre de exp : \n" + exp1.arbre(ordre_atomes));
 	}
 }
